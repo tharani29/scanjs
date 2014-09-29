153f6ecad56d1fd5d796e69f47d994cf60bae12a
Frederik B <fbraun+gh@mozilla.com>
2014-09-29 21:23:20 +0200

    more verbosity to debug auto-deploy on travis-ci

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,6 +3,7 @@
 # This makes sure, that we don't deploy pull requests to gh-pages :-)
 if [ $TRAVIS_PULL_REQUEST != false ];
 then
+  echo "Not deploying test-run for a pull request"
   exit 0
 fi
 
@@ -10,7 +11,7 @@ fi
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
- git add .
- git commit -m "Deployed to Github Pages"
+ git add -v .
+ git commit -v -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
